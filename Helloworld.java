ee42d84f59fd3ec0f10bddd04010e5165701f139
student <student@192.168.52.81>
2016-02-26 14:53:36 +0800

    Merge origin/master
    Conflicts:
    	helloworld/Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author student
+ */
+public class Helloworld {
+    private String abc;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello"); 
+         System.out.println("Hello All");
+         System.out.println("Add Branch B1");
+         System.out.println("Merge All");
+
+         System.out.println("After");
+         System.out.println("After123");
+         
+         //abc
+
+
+  
+        
+        // TODO code application logic here
+    }
+    
+}
